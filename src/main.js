611775b1efddbab0841a9a48c706bf10ad83de8d
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-01-30 16:38:28 +0100

    Recreated home page with webcomponents 3 for OSLO validator in line with the existing layout

--- src/main.js
+++ src/main.js
@@ -9,13 +9,29 @@ import {
   VlRegion,
   VlGrid,
   VlColumn,
-  VlLayout
+  VlLayout,
+  VlTabs,
+  VlTab,
+  VlUpload,
+  VlInputField,
+  VlTextarea,
+  VlTitle,
+  VlSelect,
+  VlButton
 } from '@govflanders/vl-ui-vue-components';
 
 Vue.component('vl-grid', VlGrid);
 Vue.component('vl-region', VlRegion);
 Vue.component('vl-column', VlColumn);
 Vue.component('vl-layout', VlLayout);
+Vue.component('vl-tabs', VlTabs);
+Vue.component('vl-tab', VlTab);
+Vue.component('vl-upload', VlUpload);
+Vue.component('vl-input-field', VlInputField);
+Vue.component('vl-textarea', VlTextarea);
+Vue.component('vl-title', VlTitle);
+Vue.component('vl-select', VlSelect);
+Vue.component('vl-button', VlButton);
 
 Vue.use(VlCore);
 Vue.use(VlUtil);
